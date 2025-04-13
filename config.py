d1c9af3d974d6d4903ab61212ff820413416d069
Fokstone Enterprise <83293419+farhan123mohd@users.noreply.github.com>
2025-04-13 12:42:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 # MySQL Configuration from FreeSQLDatabase.com
 MYSQL_HOST = 'sql7.freesqldatabase.com'
 MYSQL_USER = 'sql7772767'
-MYSQL_PASSWORD = 'brotherLS2125'  # Replace this with the real password from your email
+MYSQL_PASSWORD = '3CWSIs9KyU'  # Replace this with the real password from your email
 MYSQL_DB = 'sql7772767'
